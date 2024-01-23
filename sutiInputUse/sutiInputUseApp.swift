721de85cda302c25eee1b8104e2973899a8cbf55
morisum <moris@r.sannet.ne.jp>
2024-01-24 06:41:39 +0900

    Initial Commit

---
+++ sutiInputUse/sutiInputUseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sutiInputUseApp.swift
+//  sutiInputUse
+//
+//  Created by MsMacM on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct sutiInputUseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
