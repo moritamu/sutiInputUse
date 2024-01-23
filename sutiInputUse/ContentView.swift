29815598459d6c47646e6a48967bd1fd3de7601e
morisum <moris@r.sannet.ne.jp>
2024-01-24 06:53:47 +0900

    数値が、数値として入力できる。

--- sutiInputUse/ContentView.swift
+++ sutiInputUse/ContentView.swift
@@ -3,13 +3,20 @@
 //  sutiInputUse
 //
 //  Created by MsMacM on 2024/01/24.
-//
+//　sotecではないが、数値を入力するパターン
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var editNum = 0.0
+    
     var body: some View {
         VStack {
+            HStack {
+                TextField("Input Number", value: $editNum,format: .number)
+                    .keyboardType(.numberPad)
+                    .padding(.leading, 25.0)
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
